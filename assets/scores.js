681d736b6e8b43b08fe6138e5b146399bf0af251
mjlindskog <lindskogmarkj@gmail.com>
2021-05-23 23:52:20 -0400

    more js edits, JSON property issues

--- assets/scores.js
+++ assets/scores.js
@@ -1,17 +1,7 @@
-var getScores = localStorage.getItem('getScores');
-getScores.JSON.parse(getScores);
-for(i=0; i < getScores.length; i++) {
-    var scoreLi = document.createElement('li');
-    scoreLi.textContent=getScores[i].submitValue
-}
-
 var allScores = document.querySelector('.all-time-scores');
 var mainMenuBtn = document.querySelector('.main-menu-btn');
 var resetBtn = document.querySelector('.reset-btn');
 
-
-// document.getElementsByClassName('main-menu-btn').onclick = returnStart;
-// document.getElementsByClassName('reset-btn').onclick = clearAllScores;
 
 mainMenuBtn.addEventListener("click", returnStart);
 resetBtn.addEventListener("click", clearAllScores);
@@ -28,3 +18,11 @@ function returnStart() {
     console.log("Yes, the button works");
 
 }
+
+var getScores = localStorage.getItem('getScores');
+getScores = JSON.parse(getScores);
+
+    for(i=0; i < getScores.length; i++) {
+        var scoreLi = document.createElement('li');
+        scoreLi.textContent=getScores[i].submitValue
+    }
